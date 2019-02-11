6f2e618f438fcfdd03136b5d75f1330a544ae040
inPhoenix <spiderk@gmail.com>
2019-02-11 07:57:02 +0000

    Adding Update User method

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user")
+const extend = require("lodash/extend")
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -37,4 +38,21 @@ exports.getUser = (req, res) => {
   req.profile.hashed_password = undefined
   req.profile.salt = undefined
   return res.json(req.profile)
+}
+
+exports.updateUser = (req, res, next) => {
+  let user = req.profile
+  // todo: use spread instead
+  user = extend(user, req.body)
+  user.updated = Date.now()
+  user.save(err => {
+    if (err) {
+      return res.status(400).json({
+        error: "You are not authorized to perform this action"
+      })
+    }
+    user.hashed_password = undefined
+    user.salt = undefined
+    res.json({ user })
+  })
 }
--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { userById, allUsers, getUser } = require("../controllers/user")
+const { userById, allUsers, getUser, updateUser } = require("../controllers/user")
 const { requireSignin } = require("../controllers/auth")
 
 const router = express.Router()
@@ -11,6 +11,7 @@ router.get("/users", allUsers)
 // on :userId
 
 router.get("/user/:userId", requireSignin, getUser)
+router.put("/user/:userId", requireSignin, updateUser)
 
 // execute if there is :userId on the url.
 // UserById method will trigger validation
