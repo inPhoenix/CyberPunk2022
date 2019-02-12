dc353f749f6cfd7618cf785c2a5719eefbaa506a
inPhoenix <spiderk@gmail.com>
2019-02-12 06:26:39 +0000

    Adding delete user operation

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { userById, allUsers, getUser, updateUser } = require("../controllers/user")
+const { userById, allUsers, getUser, updateUser, deleteUser } = require("../controllers/user")
 const { requireSignin } = require("../controllers/auth")
 
 const router = express.Router()
@@ -12,6 +12,7 @@ router.get("/users", allUsers)
 
 router.get("/user/:userId", requireSignin, getUser)
 router.put("/user/:userId", requireSignin, updateUser)
+router.delete("/user/:userId", requireSignin, deleteUser)
 
 // execute if there is :userId on the url.
 // UserById method will trigger validation
