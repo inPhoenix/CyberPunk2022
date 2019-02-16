09491b8959108ad82a258bfc5ab39f5fca3568a2
inPhoenix <spiderk@gmail.com>
2019-02-16 10:41:08 +0000

    Adding update post method

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -1,19 +1,27 @@
 const express = require("express")
+const {
+  postById,
+  getPosts,
+  createPost,
+  postsByUser,
+  isPoster,
+  deletePost,
+  updatePost
+} = require("../controllers/post")
 const { createPostValidator } = require("../validator")
 const { userById } = require("../controllers/user")
 const { requireSignin } = require("../controllers/auth")
-
-const { getPosts, createPost, postsByUser } = require("../controllers/post")
 
 const router = express.Router()
 
 router.get("/", getPosts)
 router.post("/post/new/:userId", requireSignin, createPost, createPostValidator)
+router.get("/posts/by/:userId", postsByUser)
+router.put('/post/:postId', requireSignin, isPoster, updatePost)
+router.delete('/post/:postId', requireSignin, isPoster, deletePost)
 
-router.get('/posts/by/:userId', postsByUser)
-
-// execute if there is :userId on the url.
-// UserById method will trigger validation
+// execute if there is :param on the url.
 router.param("userId", userById)
+router.param("postId", postById)
 
 module.exports = router
