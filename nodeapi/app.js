cb456e299c5f194fe13947dd5d6a8621d2f10196
inPhoenix <spiderk@gmail.com>
2019-03-08 22:23:18 +0000

    adding stuff

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -71,6 +71,18 @@ app.use(function (err, req, res, next) {
 });
 
 // react stuff
+
+const test = true;
+
+if (test) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
 if (['production'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
