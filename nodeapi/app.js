c14dd77267089cbe14b6581c250fa5c21eac215b
inPhoenix <spiderk@gmail.com>
2019-03-08 22:35:40 +0000

    adding stuff

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -51,6 +51,9 @@ app.get('/docs', (req,res) => {
   })
 })
 
+
+console.log('process.env.NODE_ENV::::::::::::', process.env.NODE_ENV)
+
 const origin = process.env.NODE_ENV === 'production' ?
   'https://cybersocial.herokuapp.com' :
   'http://localhost:3000';
