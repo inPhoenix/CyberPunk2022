3abd8c48b9d9c4643c171eebc26474d81265ed8d
inPhoenix <spiderk@gmail.com>
2019-03-09 20:23:42 +0000

    adding stuff

--- client/src/components/homepage/Homepage.jsx
+++ client/src/components/homepage/Homepage.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react"
-import { Arwes, Col, Frame as FrameC, Header, Words } from "arwes"
+import { Arwes, Col, Content, Frame as FrameC, Header, Words } from "arwes"
 import { connect } from "react-redux"
 import { MarginTop } from "../../common/styled/MarginTop"
 
 class Homepage extends Component {
-  render() {
+  get isAuthenticated() {
+    const { user } = this.props
+    return !!user.loaded.token
+  }
+
+  notAuthorized() {
     return (
-      <Arwes style={{ marginLeft: this.props.isExpanded ? '240px' : '0' }}>
-          <MarginTop/>
+      <Arwes style={{ marginLeft: this.props.isExpanded ? "190px" : "0" }}>
+        <Content style={{ margin: "20px 50px" }}>
+          <blockquote data-layer="alert">ACCESS DENIED</blockquote>
+        </Content>
+      </Arwes>
+    )
+  }
+  render() {
+    if (!this.isAuthenticated) {
+      return this.notAuthorized()
+    }
+    return (
+      <Arwes style={{ marginLeft: this.props.isExpanded ? "240px" : "0" }}>
+        <MarginTop />
         <Col s={12} m={8} l={6} offset={["m2", "l3"]}>
           <FrameC animate level={2} corners={1} layer={"success"}>
             <h3 style={{ margin: 20 }}>
