7bcbd54e520297083f81576135be692d916dad4a
inPhoenix <spiderk@gmail.com>
2019-03-10 17:07:54 +0000

    adding stuff

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { Arwes, Content } from "arwes"
+import { Arwes, Content, Project, Words } from "arwes"
+const ASSETS = `${process.env.PUBLIC_URL}/assets`
 
 class Profile extends Component {
   state = {
-    user: '',
-    redirectToSignin: false,
+    user: "",
+    redirectToSignin: false
   }
 
   get isAuthenticated() {
-    const {user} =this.props
+    const { user } = this.props
     return !!user.loaded.token
   }
 
   notAuthorized() {
     return (
-      <Arwes style={{ marginLeft:  this.props.isExpanded ? '190px' : '0' }}>
-        <Content style={{ margin: '20px 50px' }}>
-          <blockquote data-layer='alert'>ACCESS DENIED</blockquote>
+      <Arwes style={{ marginLeft: this.props.isExpanded ? "190px" : "0" }}>
+        <Content style={{ margin: "20px 50px" }}>
+          <blockquote data-layer="alert">ACCESS DENIED</blockquote>
         </Content>
       </Arwes>
     )
@@ -25,32 +26,59 @@ class Profile extends Component {
 
   componentDidMount() {
     const getUserId = this.props.match.params.userId
-    console.log('%c getUserId', 'background: red', getUserId)
+    console.log("%c getUserId", "background: red", getUserId)
   }
 
   render() {
-    const {user} =this.props
+    const { user } = this.props
+    console.log("%c user", "background: red", user)
+    const safeUser = {
+      loaded: {
+        user: {
+          name: "",
+          email: ""
+        },
+        ...user.loaded
+      }
+    }
+
+    console.log("%c safeUser", "background: red", safeUser)
+    const getName = safeUser.loaded.user.name
+    const getEmail = safeUser.loaded.user.email
+
     if (!this.isAuthenticated) {
       return this.notAuthorized()
     }
 
-    console.log('%c user', 'background: red', user)
+    console.log("%c user", "background: red", user)
     return (
-      <div className='container'>
+      <Arwes>
+        <div style={{ margin: '100px', marginLeft: this.props.isExpanded ? "250px" : "100px" }}>
+        <Project animate header="Profile">
+          {anim => (
+            <div><Words animate show={anim.entered}>
+              {getName}
+              </Words>
+              <div>
+                <Words animate show={anim.entered}>
+                  {getEmail}
+                </Words>
+              </div>
 
-        asdfasdfasdf
+            </div>
+          )}
+        </Project>
+        </div>
+      </Arwes>
 
-
-      </div>
     )
   }
 }
 
-const mapState = (state) => {
+const mapState = state => {
   return {
     user: state.user
   }
-
 }
 
 export default connect(mapState)(Profile)
